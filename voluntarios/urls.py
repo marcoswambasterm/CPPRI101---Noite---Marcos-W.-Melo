21b741fb5210b40880dc5e8c4d8f968f61cef7b7
marcoswambasterm <marcoswambasterm@gmail.com>
2024-11-14 17:35:55 -0300

    alteração para CBV voluntarios

--- voluntarios/urls.py
+++ voluntarios/urls.py
@@ -2,33 +2,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('voluntarios/', views.lista_voluntarios, name='lista_voluntarios'),
-    path('voluntarios/medicos/', views.listar_medicos, name='lista_medicos'),
-    path('voluntarios/psicologos/', views.listar_psicologos, name='lista_psicologos'),
-    path('voluntarios/veterinarios/', views.listar_veterinarios, name='lista_veterinarios'),
-    path('voluntarios/atendentes/', views.listar_atendentes, name='lista_atendentes'),
+    path('voluntarios/', views.ListaVoluntariosView.as_view(), name='lista_voluntarios'),
 
     # URLs para Médicos
-    path('medicos/', views.listar_medicos, name='listar_medicos'),
-    path('medicos/adicionar/', views.adicionar_medico, name='adicionar_medico'),
-    path('medicos/editar/<int:pk>/', views.editar_medico, name='editar_medico'),
-    path('medicos/excluir/<int:pk>/', views.excluir_medico, name='excluir_medico'),
+    path('medicos/', views.ListaMedicosView.as_view(), name='lista_medicos'),
+    path('medicos/adicionar/', views.AdicionarMedicoView.as_view(), name='adicionar_medico'),
+    path('medicos/editar/<int:pk>/', views.EditarMedicoView.as_view(), name='editar_medico'),
+    path('medicos/excluir/<int:pk>/', views.ExcluirMedicoView.as_view(), name='excluir_medico'),
 
     # URLs para Psicólogos
-    path('psicologos/', views.listar_psicologos, name='listar_psicologos'),
-    path('psicologos/adicionar/', views.adicionar_psicologo, name='adicionar_psicologo'),
-    path('psicologos/editar/<int:pk>/', views.editar_psicologo, name='editar_psicologo'),
-    path('psicologos/excluir/<int:pk>/', views.excluir_psicologo, name='excluir_psicologo'),
+    path('psicologos/', views.ListaPsicologosView.as_view(), name='listar_psicologos'),
+    path('psicologos/adicionar/', views.AdicionarPsicologoView.as_view(), name='adicionar_psicologo'),
+    path('psicologos/editar/<int:pk>/', views.EditarPsicologoView.as_view(), name='editar_psicologo'),
+    path('psicologos/excluir/<int:pk>/', views.ExcluirPsicologoView.as_view(), name='excluir_psicologo'),
 
     # URLs para Veterinários
-    path('veterinarios/', views.listar_veterinarios, name='listar_veterinarios'),
-    path('veterinarios/adicionar/', views.adicionar_veterinario, name='adicionar_veterinario'),
-    path('veterinarios/editar/<int:pk>/', views.editar_veterinario, name='editar_veterinario'),
-    path('veterinarios/excluir/<int:pk>/', views.excluir_veterinario, name='excluir_veterinario'),
+    path('veterinarios/', views.ListaVeterinariosView.as_view(), name='listar_veterinarios'),
+    path('veterinarios/adicionar/', views.AdicionarVeterinarioView.as_view(), name='adicionar_veterinario'),
+    path('veterinarios/editar/<int:pk>/', views.EditarVeterinarioView.as_view(), name='editar_veterinario'),
+    path('veterinarios/excluir/<int:pk>/', views.ExcluirVeterinarioView.as_view(), name='excluir_veterinario'),
 
     # URLs para Atendentes
-    path('atendentes/', views.listar_atendentes, name='listar_atendentes'),
-    path('atendentes/adicionar/', views.adicionar_atendente, name='adicionar_atendente'),
-    path('atendentes/editar/<int:pk>/', views.editar_atendente, name='editar_atendente'),
-    path('atendentes/excluir/<int:pk>/', views.excluir_atendente, name='excluir_atendente'),
+    path('atendentes/', views.ListaAtendentesView.as_view(), name='lista_atendentes'),
+    path('atendentes/adicionar/', views.AdicionarAtendenteView.as_view(), name='adicionar_atendente'),
+    path('atendentes/editar/<int:pk>/', views.EditarAtendenteView.as_view(), name='editar_atendente'),
+    path('atendentes/excluir/<int:pk>/', views.ExcluirAtendenteView.as_view(), name='excluir_atendente'),
 ]
--- voluntarios/views.py
+++ voluntarios/views.py
@@ -1,152 +1,113 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Medico, Psicologo, Veterinario, Atendente
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Medico, Psicologo, Veterinario, Atendente, Voluntario
 from .forms import MedicoForm, PsicologoForm, VeterinarioForm, AtendenteForm
 
-def lista_voluntarios(request):
-    # Obter a contagem de cada tipo de voluntário
-    total_medicos = Medico.objects.count()
-    total_psicologos = Psicologo.objects.count()
-    total_veterinarios = Veterinario.objects.count()
-    total_atendentes = Atendente.objects.count()
-    total_voluntarios = total_medicos + total_psicologos + total_veterinarios + total_atendentes
+# Lista Geral de Voluntários
+class ListaVoluntariosView(ListView):
+    queryset = Medico.objects.all()
+    template_name = 'lista_voluntarios.html'
+    context_object_name = 'voluntarios'
 
-    context = {
-        'total_medicos': total_medicos,
-        'total_psicologos': total_psicologos,
-        'total_veterinarios': total_veterinarios,
-        'total_atendentes': total_atendentes,
-        'total_voluntarios': total_voluntarios,
-    }
-    return render(request, 'lista_voluntarios.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_medicos'] = Medico.objects.count()
+        context['total_psicologos'] = Psicologo.objects.count()
+        context['total_veterinarios'] = Veterinario.objects.count()
+        context['total_atendentes'] = Atendente.objects.count()
+        context['total_voluntarios'] = (
+            context['total_medicos'] + context['total_psicologos'] + context['total_veterinarios'] + context['total_atendentes']
+        )
+        return context
 
-# Médicos
-def listar_medicos(request):
-    medicos = Medico.objects.all()
-    return render(request, './lista_medicos.html', {'medicos': medicos})
+# Views de Médicos
+class ListaMedicosView(ListView):
+    model = Medico
+    template_name = 'lista_medicos.html'
+    context_object_name = 'medicos'
 
-def adicionar_medico(request):
-    if request.method == 'POST':
-        form = MedicoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_medicos')
-    else:
-        form = MedicoForm()
-    return render(request, './form_medico.html', {'form': form})
+class AdicionarMedicoView(CreateView):
+    model = Medico
+    form_class = MedicoForm
+    template_name = 'form_medico.html'
+    success_url = reverse_lazy('lista_medicos')
 
-def editar_medico(request, pk):
-    medico = get_object_or_404(Medico, pk=pk)
-    if request.method == 'POST':
-        form = MedicoForm(request.POST, instance=medico)
-        if form.is_valid():
-            form.save()
-            return redirect('lista_medicos')
-    else:
-        form = MedicoForm(instance=medico)
-    return render(request, './form_medico.html', {'form': form})
+class EditarMedicoView(UpdateView):
+    model = Medico
+    form_class = MedicoForm
+    template_name = 'form_medico.html'
+    success_url = reverse_lazy('lista_medicos')
 
-def excluir_medico(request, pk):
-    medico = get_object_or_404(Medico, pk=pk)
-    if request.method == "POST":
-        medico.delete()
-        return redirect('listar_medicos')
-    return render(request, './confirm_delete_medicos.html', {'obj': medico})
+class ExcluirMedicoView(DeleteView):
+    model = Medico
+    template_name = 'confirm_delete_medicos.html'
+    success_url = reverse_lazy('lista_medicos')
 
-# Psicólogos
-def listar_psicologos(request):
-    psicologos = Psicologo.objects.all()
-    return render(request, './lista_psicologos.html', {'psicologos': psicologos})
+# Views de Psicólogos
+class ListaPsicologosView(ListView):
+    model = Psicologo
+    template_name = 'lista_psicologos.html'
+    context_object_name = 'psicologos'
 
-def adicionar_psicologo(request):
-    if request.method == 'POST':
-        form = PsicologoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_psicologos')
-    else:
-        form = PsicologoForm()
-    return render(request, './form_psicologo.html', {'form': form})
+class AdicionarPsicologoView(CreateView):
+    model = Psicologo
+    form_class = PsicologoForm
+    template_name = 'form_psicologo.html'
+    success_url = reverse_lazy('listar_psicologos')
 
-def editar_psicologo(request, pk):
-    psicologo = get_object_or_404(Psicologo, pk=pk)
-    if request.method == 'POST':
-        form = PsicologoForm(request.POST, instance=psicologo)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_psicologos')
-    else:
-        form = PsicologoForm(instance=psicologo)
-    return render(request, './form_psicologo.html', {'form': form})
+class EditarPsicologoView(UpdateView):
+    model = Psicologo
+    form_class = PsicologoForm
+    template_name = 'form_psicologo.html'
+    success_url = reverse_lazy('listar_psicologos')
 
-def excluir_psicologo(request, pk):
-    psicologo = get_object_or_404(Psicologo, pk=pk)
-    if request.method == "POST":
-        psicologo.delete()
-        return redirect('listar_psicologos')
-    return render(request, './confirm_delete_psicologos.html', {'obj': psicologo})
+class ExcluirPsicologoView(DeleteView):
+    model = Psicologo
+    template_name = 'confirm_delete_psicologos.html'
+    success_url = reverse_lazy('listar_psicologos')
 
-# Veterinários
-def listar_veterinarios(request):
-    veterinarios = Veterinario.objects.all()
-    return render(request, './lista_veterinarios.html', {'veterinarios': veterinarios})
+# Views de Veterinários
+class ListaVeterinariosView(ListView):
+    model = Veterinario
+    template_name = 'lista_veterinarios.html'
+    context_object_name = 'veterinarios'
 
-def adicionar_veterinario(request):
-    if request.method == 'POST':
-        form = VeterinarioForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_veterinarios')
-    else:
-        form = VeterinarioForm()
-    return render(request, './form_veterinario.html', {'form': form})
+class AdicionarVeterinarioView(CreateView):
+    model = Veterinario
+    form_class = VeterinarioForm
+    template_name = 'form_veterinario.html'
+    success_url = reverse_lazy('listar_veterinarios')
 
-def editar_veterinario(request, pk):
-    veterinario = get_object_or_404(Veterinario, pk=pk)
-    if request.method == 'POST':
-        form = VeterinarioForm(request.POST, instance=veterinario)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_veterinarios')
-    else:
-        form = VeterinarioForm(instance=veterinario)
-    return render(request, './form_veterinario.html', {'form': form})
+class EditarVeterinarioView(UpdateView):
+    model = Veterinario
+    form_class = VeterinarioForm
+    template_name = 'form_veterinario.html'
+    success_url = reverse_lazy('listar_veterinarios')
 
-def excluir_veterinario(request, pk):
-    veterinario = get_object_or_404(Veterinario, pk=pk)
-    if request.method == "POST":
-        veterinario.delete()
-        return redirect('listar_veterinarios')
-    return render(request, './confirm_delete_veterinarios.html', {'obj': veterinario})
+class ExcluirVeterinarioView(DeleteView):
+    model = Veterinario
+    template_name = 'confirm_delete_veterinarios.html'
+    success_url = reverse_lazy('listar_veterinarios')
 
-# Atendentes
-def listar_atendentes(request):
-    atendentes = Atendente.objects.all()
-    return render(request, './lista_atendentes.html', {'atendentes': atendentes})
+# Views de Atendentes
+class ListaAtendentesView(ListView):
+    model = Atendente
+    template_name = 'lista_atendentes.html'
+    context_object_name = 'atendentes'
 
-def adicionar_atendente(request):
-    if request.method == 'POST':
-        form = AtendenteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_atendentes')
-    else:
-        form = AtendenteForm()
-    return render(request, './form_atendente.html', {'form': form})
+class AdicionarAtendenteView(CreateView):
+    model = Atendente
+    form_class = AtendenteForm
+    template_name = 'form_atendente.html'
+    success_url = reverse_lazy('lista_atendentes')
 
-def editar_atendente(request, pk):
-    atendente = get_object_or_404(Atendente, pk=pk)
-    if request.method == 'POST':
-        form = AtendenteForm(request.POST, instance=atendente)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_atendentes')
-    else:
-        form = AtendenteForm(instance=atendente)
-    return render(request, './form_atendente.html', {'form': form})
+class EditarAtendenteView(UpdateView):
+    model = Atendente
+    form_class = AtendenteForm
+    template_name = 'form_atendente.html'
+    success_url = reverse_lazy('lista_atendentes')
 
-def excluir_atendente(request, pk):
-    atendente = get_object_or_404(Atendente, pk=pk)
-    if request.method == "POST":
-        atendente.delete()
-        return redirect('listar_atendentes')
-    return render(request, './confirm_delete_atendentes.html', {'obj': atendente})
+class ExcluirAtendenteView(DeleteView):
+    model = Atendente
+    template_name = 'confirm_delete_atendentes.html'
+    success_url = reverse_lazy('lista_atendentes')
